962ac8bcc8ab59e49dd479c042d3458d7856f529
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2021-04-30 19:27:23 +0100

    Deleted all build files and added gitignore

--- CryptradeNotif/Commands/TestCommands.cs
+++ CryptradeNotif/Commands/TestCommands.cs
@@ -26,17 +26,29 @@ namespace CryptradeNotif.Modules {
         [Summary("Watches a token. Usage: watch {address} {priceUsd}")]
         public async Task WatchAsync(
             [Summary("The token address")] string address, decimal priceUsd) {
-            // We can also access the channel from the Command Context.
 
-            var token = new WatchedToken(this.Context.Message.Author.Id, address, priceUsd);
-            _tokenWatcher.AddToken(token);
+            var tokenAttempt = await this._tokenRequester.GetTokenData(address);
+            if ( tokenAttempt == null ) {
+                var embed = new EmbedBuilder {
+                    Title = "Address does not exist",
+                    Description = $"Address **{address}** does not exist on BscScan!",
+                    Color = Color.Red
+                };
 
-            var embed = new EmbedBuilder {
-                Title = "Address added to watchlist",
-                Description = $"**{address}** has been added to the watchlist"
-            }.WithCurrentTimestamp();;
+                await this.ReplyAsync(embed: embed.Build());
+            } else {
+                // We can also access the channel from the Command Context.
+                var token = new WatchedToken(this.Context.Message.Author.Id, address, priceUsd);
+                _tokenWatcher.AddToken(token);
 
-            await this.ReplyAsync(embed: embed.Build());
+                var embed = new EmbedBuilder {
+                    Title = "Address added to watchlist",
+                    Description = $"**{address}** has been added to the watchlist",
+                    Color = Color.Green
+                }.WithCurrentTimestamp();;
+
+                await this.ReplyAsync(embed: embed.Build());   
+            }
         }
 
         [Command("unwatch")]
@@ -49,7 +61,8 @@ namespace CryptradeNotif.Modules {
 
                 var embed = new EmbedBuilder {
                     Title = "Address watchlist cleared",
-                    Description = "All addresses have been removed from the watchlist."
+                    Description = "All addresses have been removed from the watchlist.",
+                    Color = Color.Green
                 }.WithCurrentTimestamp();;
 
                 await this.ReplyAsync(embed: embed.Build());
@@ -59,7 +72,8 @@ namespace CryptradeNotif.Modules {
 
                 var embed = new EmbedBuilder {
                     Title = "Address removed from watchlist",
-                    Description = $"Address **{address}** is no longer being watched."
+                    Description = $"Address **{address}** is no longer being watched.",
+                    Color = Color.Green
                 }.WithCurrentTimestamp();;
 
                 await this.ReplyAsync(embed: embed.Build());
@@ -70,11 +84,13 @@ namespace CryptradeNotif.Modules {
         [Summary("Lists all currently watched tokens")]
         public async Task ListWatchlistAsync() {
             var watchlist = _tokenWatcher.GetWatchedTokens();
+            var exists = watchlist.Any();
             var embed = new EmbedBuilder() {
                 Title = "Watchlist",
-                Description = watchlist.Any()
+                Description = exists
                     ? string.Join("\n", _tokenWatcher.GetWatchedTokens())
-                    : "No tokens watched."
+                    : "No tokens watched.",
+                Color = exists ? Color.Green : Color.Orange
             }.WithCurrentTimestamp();;
 
             await this.ReplyAsync(embed: embed.Build());
@@ -85,14 +101,20 @@ namespace CryptradeNotif.Modules {
         public async Task TokenStatsAsync([Summary("Coin address")] string address) {
             var token = await _tokenRequester.GetTokenData(address);
 
-            var embed = new EmbedBuilder()
-                .WithTitle("Token Data")
-                .AddField("Name", token.Name)
-                .AddField("Price", $"${token.PriceUsd}")
-                .AddField("Liquidity", $"${token.LiquidityUsd}")
-                .AddField("Network", token.Network)
-                .WithDescription(new TokenAddress(token.Id).AsEmbed());
-            
+            var embed = token == null
+                ? new EmbedBuilder()
+                    .WithTitle("Token does not exist")
+                    .WithDescription($"Token with address **{address}** does not exist.")
+                    .WithColor(Color.Red)
+                : new EmbedBuilder()
+                    .WithTitle("Token Data")
+                    .AddField("Name", token.Name)
+                    .AddField("Price", $"${token.PriceUsd}")
+                    .AddField("Liquidity", $"${token.LiquidityUsd}")
+                    .AddField("Network", token.Network)
+                    .WithDescription(new TokenAddress(token.Id).AsEmbed())
+                    .WithColor(Color.Green);
+
             embed.WithCurrentTimestamp();
 
             await ReplyAsync(embed: embed.Build());
@@ -111,6 +133,23 @@ namespace CryptradeNotif.Modules {
             }
 
             await ReplyAsync("Here's a list of commands and their description: ", false, embedBuilder.Build());
+        }
+
+        [Command("test")]
+        public async Task RunTests() {
+            try {
+                await this.HelpAsync();
+                await this.TokenStatsAsync("0x8244609023097AeF71C702cCbaEFC0bde5b48694");
+                await this.TokenStatsAsync("0x8244609023097AeF71C702cCbaEAB1bde5b48694");
+                await this.ListWatchlistAsync();
+                await this.WatchAsync("0x8244609023097AeF71C702cCbaEFC0bde5b48694", (decimal) 10.00);
+                await this.WatchAsync("0x8244609023097AeF71C702cCbaEAB1bde5b48694", (decimal) 10.00f);
+                await this.UnwatchAsync("0x8244609023097AeF71C702cCbaEFC0bde5b48694");
+                await this.UnwatchAsync("0x8244609023097AeF71C702cCbaEAB1bde5b48694");
+                await this.UnwatchAsync("NULL");
+            } catch ( Exception e ) {
+                await this.ReplyAsync($"Failed to successfully run all tests: {e}");
+            }
         }
     }
 }
--- CryptradeNotif/Services/TokenRequests.cs
+++ CryptradeNotif/Services/TokenRequests.cs
@@ -63,8 +63,7 @@ namespace CryptradeNotif.Services {
                 var tokenData = await this.GetTokenData(token.Address);
                 var watchedToken = this._tokenWatcher.GetWatchedToken(token.Address);
 
-                if ( watchedToken == null || tokenData == null )
-                    continue;
+                if ( watchedToken == null || tokenData == null ) continue;
 
                 if ( tokenData.PriceUsd > watchedToken.TargetPrice ) {
                     var embed = new EmbedBuilder() {
